423d942ab2d7de1eab3b6ce720f23d9e150d4272
kavitalearns <kavitabansal_01@yahoo.co.in>
2021-01-23 23:09:04 -0600

    working navbar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { PatternsComponent } from './patterns/patterns.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes=[
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:HomeComponent},
+  { path: 'patterns', component:PatternsComponent},
+  { path: 'shopping-list', component:ShoppingListComponent },
+  { path: 'contact-us', component:ContactUsComponent }
+];
+@NgModule({
+imports: [RouterModule.forRoot(appRoutes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PatternsComponent } from './patterns/patterns.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatternsComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
